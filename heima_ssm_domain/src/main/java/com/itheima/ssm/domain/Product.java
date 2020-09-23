90e15ce288ef0129cba8064ce93261aa44a75715
mo jing <251419156@qq.com>
2020-09-23 13:02:17 +0800

    1

---
+++ heima_ssm_domain/src/main/java/com/itheima/ssm/domain/Product.java
@@ -0,0 +1,48 @@
+package com.itheima.ssm.domain;
+
+
+
+
+import com.itheima.ssm.utils.DateUtils;
+import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+/**
+ * 产品信息
+ */
+@Data
+public class Product {
+    private String id; // 主键
+    private String productNum; // 编号 唯一
+    private String productName; // 名称
+    private String cityName; // 出发城市
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private Date departureTime; // 出发时间
+    private String departureTimeStr;
+    private double productPrice; // 产品价格
+    private String productDesc; // 产品描述
+    private Integer productStatus; // 状态 0 关闭 1 开启
+    private String productStatusStr;
+
+    public String getProductStatusStr() {
+        if (productStatus != null){
+           //状态 0 关闭 1 开启
+            if (productStatus == 0){
+                productStatusStr = "关闭";
+            }
+            if (productStatus == 1){
+                productStatusStr = "开启";
+            }
+        }
+        return productStatusStr;
+    }
+
+    public String getDepartureTimeStr() {
+        if(departureTime != null){
+            departureTimeStr = DateUtils.date2String(departureTime,"yyyy-MM-dd HH:mm:ss");
+        }
+        return departureTimeStr;
+    }
+}
